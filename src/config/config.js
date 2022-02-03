fadfe53e1ec0f4c5c2f653dd1d0fa5b81ca3779b
Hugo <blackfoxmmm@gmail.com>
2022-02-03 16:48:19 +0500

    ffs

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 const PG = {
-  connectionString: "postgres://postgres:68763239@localhost:5432/klinika",
+  // connectionString: "postgres://postgres:68763239@localhost:5432/klinika",
   connectionElString:
     "postgres://fujkdnij:XBr507efxLcM1TsQ1e9u0OQkkm2Bz6hS@tyke.db.elephantsql.com/fujkdnij",
 };
